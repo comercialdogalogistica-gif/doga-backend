717a3b4fb996ac77cf124ab5e4a8c69c5d2e935c
comercialdogalogistica-gif <comercial.dogalogistica@gmail.com>
2026-04-28 12:47:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,8 @@ async function enviarMensagem(telefone, nome) {
 app.post('/webhook', async (req, res) => {
   try {
     const fields = req.body.data.fields;
-console.log('FIELDS COMPLETO:', JSON.stringify(fields, null, 2));
+    console.log('FIELDS COMPLETO:', JSON.stringify(fields, null, 2));
+
     const getValue = (label) => {
       const field = fields.find(f => f.label === label);
       if (!field) return '';
